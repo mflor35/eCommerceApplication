fdeb83c02f9b381cb1dae72972b21ca2adef7661
Miguel Flores Silverio <floresmigu3l@gmail.com>
2019-12-20 10:01:11 -0800

    feat() - add password and confirm password fields when creating an user

--- src/main/java/com/example/demo/model/requests/CreateUserRequest.java
+++ src/main/java/com/example/demo/model/requests/CreateUserRequest.java
@@ -7,6 +7,36 @@ public class CreateUserRequest {
 	@JsonProperty
 	private String username;
 
+	@JsonProperty
+	private String password;
+
+	@JsonProperty
+	private String confirmPassword;
+	/**
+	 * @return the confirmPassword
+	 */
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	/**
+	 * @param confirmPassword the confirmPassword to set
+	 */
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public String getUsername() {
 		return username;
 	}
